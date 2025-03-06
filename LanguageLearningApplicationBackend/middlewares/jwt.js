b239e4238b6fb59801e40f3f0b68a7c5df57e6a0
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-06 22:19:05 +0530

    added jsonwebtoken and added jwt.js in middleware folder with jwet verification code

--- LanguageLearningApplicationBackend/middlewares/jwt.js
+++ LanguageLearningApplicationBackend/middlewares/jwt.js
@@ -1,1 +1,17 @@
 const jwt = require('jsonwebtoken')
+
+export const verifytoken= async(req,res,next)=>{
+    let token=req.headers.token;
+    try{
+    if(!token) throw 'Unauthorized access';
+    else{
+        let payload=jwt.verify(token,process.env.JWT_KEY);
+        if(!payload) throw 'Unauthorized access';
+        next()
+    }
+}  catch(error) {
+    console.log(error);
+
+}
+
+}
