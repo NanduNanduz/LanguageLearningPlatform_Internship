4307fb1d0373080242eba2cb06835489f0d021db
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-06 23:14:35 +0530

    format changed for index.js

--- LanguageLearningApplicationBackend/index.js
+++ LanguageLearningApplicationBackend/index.js
@@ -1,19 +1,16 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
-require("./config/db");
-const app = new express();
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import db from "./config/db.js"; // Ensure this is also using ES6 import
+import authRoutes from "./routes/authRoutes.js"; // Updated to ES6 import
+
+dotenv.config();
+const app = express();
 app.use(morgan('dev'));
 app.use(cors());
+db();
 
-
-const userRoutes = require("./routes/userRoutes");
-
-
-
-
-
-
+app.use('/auth', authRoutes); // Ensure the route prefix is correct
 
 app.listen(process.env.PORT, () => { console.log(`Server is running on port ${process.env.PORT}`); });
