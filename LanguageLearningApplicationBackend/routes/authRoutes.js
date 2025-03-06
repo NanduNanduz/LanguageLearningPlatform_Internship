5a6ef7e8c3eee6420155c29e23ed6a1c1c33c742
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-06 23:15:18 +0530

    deleted checkavailability function

--- LanguageLearningApplicationBackend/routes/authRoutes.js
+++ LanguageLearningApplicationBackend/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { register, login, checkAvailability} from "../controllers/authController.js";
+import { register, login} from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register",register)
-router.post("/login",login)
-router.post("/check-availability", checkAvailability); 
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+
+router.post("/register", register);
+router.post("/login", login);
+
 
 export default router;
