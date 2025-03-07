0e22d45a7760b3ddac4aa2d72659c819c5bb8afe
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-07 14:14:29 +0530

    added confirm password while registering

--- LanguageLearningApplicationBackend/controllers/authController.js
+++ LanguageLearningApplicationBackend/controllers/authController.js
@@ -3,7 +3,12 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, confirmPassword } = req.body;
+
+    // Check if password and confirmPassword match
+    if (password !== confirmPassword) {
+        return res.status(400).json({ message: "Passwords do not match." });
+    }
 
     // Check if the username already exists
     const existingUser = await userModel.findOne({ name });
@@ -16,25 +21,27 @@ export const register = async (req, res, next) => {
     if (existingEmail) {
         return res.status(400).json({ message: "Email already registered." });
     }
-    
+
     try {
         // Hash the password using bcrypt (async version)
         const hash = await bcrypt.hash(password, 5);
 
         // Create a new user with hashed password
         const newUser = new userModel({
-            ...req.body, // Spread all user details
-            password: hash, // Replace plain password with hash
+            name,
+            email,
+            password: hash, // Store the hashed password
         });
 
         // Save the user to the database
         await newUser.save();
 
-        res.status(201).send("User has been created");
+        res.status(201).json({ message: "User has been created" });
     } catch (err) {
         next(err);
     }
-}
+};
+
 
 //LOGIN FUNCTIONALITY
 export const login = async (req, res, next) => {
