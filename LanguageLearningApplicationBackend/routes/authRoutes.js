e95049dcdb0f8cd1ebbfe3da35a54530fe922b3b
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-07 23:41:50 +0530

    newpassword creating function added to route

--- LanguageLearningApplicationBackend/routes/authRoutes.js
+++ LanguageLearningApplicationBackend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, resetPassword, verifyOtp } from "../controllers/authController.js";
+import { register, login, resetPassword, verifyOtp, newPass } from "../controllers/authController.js";
 
 const router = express.Router();
 
@@ -11,6 +11,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.post('/reset-password', resetPassword);
 router.post('/verifyOtp',verifyOtp)
+router.post('/newPass',newPass)
 
 
 export default router;
