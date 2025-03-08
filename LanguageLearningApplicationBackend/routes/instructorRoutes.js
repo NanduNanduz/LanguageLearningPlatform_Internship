1856285c065e700ac53bbbd89593335ad97fd8d4
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-08 12:50:15 +0530

    called createcourse to instructorRoutes

--- LanguageLearningApplicationBackend/routes/instructorRoutes.js
+++ LanguageLearningApplicationBackend/routes/instructorRoutes.js
@@ -1,17 +1,13 @@
-const express = require("express");
+import express from "express";
+import {createCourse} from "../controllers/instructorController.js"
+
 const router = express.Router();
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 
+router.post("/create-course", createCourse);
 
 
-
-
-
-
-
-
-
-module.exports = router ;
+export default router;
