09ef953033b21ebb8212ab973ee1f5b230486c0b
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-09 14:23:03 +0530

    added new route to index

--- LanguageLearningApplicationBackend/index.js
+++ LanguageLearningApplicationBackend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import db from "./config/db.js"; // Ensure this is also using ES6 import
 import authRoutes from "./routes/authRoutes.js"; // Updated to ES6 import
 import instructorRoutes from "./routes/instructorRoutes.js"
+import studentAndInstructorRoutes from "./routes/studentAndInstructorRoutes.js"
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 db(); // calling db
 
 app.use('/auth', authRoutes); // Ensure the route prefix is correct
-app.use('/instructor',instructorRoutes);
+app.use('/instructor',instructorRoutes); // all function of instructor
+app.use('/user',studentAndInstructorRoutes)
 
 app.listen(process.env.PORT, () => { console.log(`Server is running on port ${process.env.PORT}`); });
