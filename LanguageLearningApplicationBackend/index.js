9168c78a020ba2fd84a1e75493bbfdd48ca7ade6
NanduNanduz <nandunsb99@gmail.com>
2025-03-10 16:02:35 +0530

    Updated the index.js with the  adminRoutes

--- LanguageLearningApplicationBackend/index.js
+++ LanguageLearningApplicationBackend/index.js
@@ -6,6 +6,9 @@ import db from "./config/db.js"; // Ensure this is also using ES6 import
 import authRoutes from "./routes/authRoutes.js"; // Updated to ES6 import
 import instructorRoutes from "./routes/instructorRoutes.js"
 import studentAndInstructorRoutes from "./routes/studentAndInstructorRoutes.js"
+import adminRoutes from "./routes/adminRoutes.js";
+
+
 
 dotenv.config();
 const app = express();
@@ -16,5 +19,6 @@ db(); // calling db
 app.use('/auth', authRoutes); // Ensure the route prefix is correct
 app.use('/instructor',instructorRoutes); // all function of instructor
 app.use('/user',studentAndInstructorRoutes)
+app.use("/admin", adminRoutes);
 
 app.listen(process.env.PORT, () => { console.log(`Server is running on port ${process.env.PORT}`); });
