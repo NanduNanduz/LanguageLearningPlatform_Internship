0462ff7fd8dde02ba2c5063beed4565c43735ea6
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-10 17:25:39 +0530

    update multer of file uploads

--- LanguageLearningApplicationBackend/utils/multer.js
+++ LanguageLearningApplicationBackend/utils/multer.js
@@ -2,18 +2,11 @@ import multer from "multer";
 import fs from "fs";
 import path from "path";
 
-// Ensure the uploads folder exists
-const uploadDir = "uploads/";
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
+
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir); // Ensure files are saved in the correct folder
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+  filename:function(req,file,callback){
+    callback(null,file.originalname);
   }
 });
 
