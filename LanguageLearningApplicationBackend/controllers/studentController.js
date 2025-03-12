25877a50af63743ad8ee62d15206b3a50968cd55
NanduNanduz <nandunsb99@gmail.com>
2025-03-12 23:41:19 +0530

    updated route and controller to get each student details in studentController and studentRoutes

--- LanguageLearningApplicationBackend/controllers/studentController.js
+++ LanguageLearningApplicationBackend/controllers/studentController.js
@@ -76,9 +76,31 @@ import dotenv from "dotenv";
 //   }
 // };
 
-
-
 dotenv.config();
+
+
+
+export const getUserDetails = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await userModel
+      .findById(userId)
+      .populate("enrolledCourses.courseId")
+      .populate("favourites")
+      .populate("certificates.courseId");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
--- LanguageLearningApplicationBackend/routes/studentRoutes.js
+++ LanguageLearningApplicationBackend/routes/studentRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { enrollCourse } from "../controllers/studentController.js";
+import {getUserDetails} from "../controllers/studentController.js"
 // import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Student enrollment route
+router.get("/single-user/:userId", getUserDetails);
 router.put("/enroll/:courseId", enrollCourse);
 
 export default router;
