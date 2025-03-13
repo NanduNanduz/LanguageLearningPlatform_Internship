b2c0960d5e63bbdd80ac9074baeaf0ece35a2f02
NanduNanduz <nandunsb99@gmail.com>
2025-03-13 16:25:23 +0530

    Updated App.jsx with exact route of login , signup and Home

--- LanguageLearningApplicationFrontend/src/App.jsx
+++ LanguageLearningApplicationFrontend/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
-const App = () => {
+function App() {
+
   return (
-    <div>
-      
-      
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />     
+    </Routes>
+  );
 }
 
-export default App
+export default App;
