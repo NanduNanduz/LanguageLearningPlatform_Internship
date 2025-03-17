3889ab21f88669257cb02e601fb73cbfee202901
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-03-17 14:31:03 +0530

    controller for fetching one student details

--- LanguageLearningApplicationBackend/controllers/studentController.js
+++ LanguageLearningApplicationBackend/controllers/studentController.js
@@ -297,3 +297,23 @@ export const getUserQuizResults = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+//GET route for fetching one student details
+
+export const getStudentDetails = async (req, res) => {
+  try {
+    const { studentId } = req.params;
+
+    const student = await userModel
+      .findOne({ _id: studentId, role: "student" })
+
+    if (!student) {
+      return res.status(404).json({ message: "student not found" });
+    }
+
+    res.status(200).json(student);
+  } catch (error) {
+    console.error("Error fetching student details:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
