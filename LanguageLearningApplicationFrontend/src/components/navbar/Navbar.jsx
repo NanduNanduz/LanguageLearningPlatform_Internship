f5ff6c6d6a4ebd8ec907bd42e267d6955b694243
Ajin Raj R V <ajinrajrv@gmail.com>
2025-03-27 15:17:28 +0530

    updated Navbar and AddCourse page

--- LanguageLearningApplicationFrontend/src/components/navbar/Navbar.jsx
+++ LanguageLearningApplicationFrontend/src/components/navbar/Navbar.jsx
@@ -406,7 +406,7 @@ const Navbar = ({ currentUser, setCurrentUser }) => {
                   </span>
 
                   {currentUser.role === "instructor" && (
-                    <Link className="link" to="/addCourse">
+                    <Link className="link" to="/addCourse"  state={{ user: currentUser }}>
                       Add Course
                     </Link>
                   )}
