b908ba4df31e5de2128e7fcd00ac9c34dbc2ee5f
NanduNanduz <nandunsb99@gmail.com>
2025-03-31 12:39:53 +0530

    Updated index

--- LanguageLearningApplicationBackend/index.js
+++ LanguageLearningApplicationBackend/index.js
@@ -94,15 +94,10 @@ app.use(morgan("dev"));
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
-      const msg =
-        "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
+      return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
